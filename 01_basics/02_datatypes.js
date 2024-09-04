c328446606fe009a73c57da88d98b78edd8c95fc
shreeyanshm <106217517+shreeyanshm@users.noreply.github.com>
2024-09-04 11:56:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //consider all the code according to the newer version of javascript
+
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3
+    +3) //readability should be high
+
+
+console.log("Hello")
+
+let name="Shreyansh" //string
+let age=18  //number
+let state=null;
+
+//number => 2 to the power 53
+//bigint =>store very large numbers
+//string => ""
+//boolean => true/false
+//null=>standalone value
+//undefined
+//symbol => uniqueness
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof null)    //noticable
+console.log(typeof undefined)    //output as undefined
