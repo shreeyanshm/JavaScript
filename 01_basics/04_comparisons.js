29df719a8b7f78d6a26df1191ffaa1cd4b60e995
shreeyanshm <106217517+shreeyanshm@users.noreply.github.com>
2024-09-11 03:35:58 +0000

    comparisons on datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2>=2)
+// console.log(2==2)
+// console.log(2!=3)
+// console.log(3>8)
+// console.log(3<2)
+
+console.log(null>0)
+console.log(null ==0)
+console.log(null >=0)
+
+console.log("2" ==2)
+console.log(2 != "1")
+
+
+//=== strict check
+
+console.log("2" === 2)
