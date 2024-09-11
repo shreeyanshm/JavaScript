9958e6523a6952bf52f6e69ce93ad2a7d3c16b28
shreeyanshm <106217517+shreeyanshm@users.noreply.github.com>
2024-09-11 03:54:23 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+//Javascript is a dynamically typed language
+
+//Primitive
+
+//7 types: number, string, boolean, null, undefined, symbol, BigInt
+
+const score=100
+
+const OutsideTemp=null
+let email; //let email=undefined
+
+const id= Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id===anotherid)
+
+
+//Refernce type => Non-primitive
+
+//Arrays, Objects, Functions
+
+const heroes=["Shaktiman","naagraj","chhota bheem"]
+
+let myObj={
+    name:"Shreeyansh",
+    age:22
+}
+
+const myfucntion=function(){
+    console.log("Hello world")
+}
