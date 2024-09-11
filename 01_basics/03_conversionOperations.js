4c196f19713f4006e4f534502e7e6239d5c22374
shreeyanshm <106217517+shreeyanshm@users.noreply.github.com>
2024-09-11 02:47:06 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score=false
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueinNumber=Number(score)
+
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1,  false => 0
+// null => 0
+//undefined => NaN
+
+
+let isLoggedIn=""
+
+let BooleanIsloggedIn=Boolean(isLoggedIn)
+// console.log(BooleanIsloggedIn)
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "anything" => true
+
+let number=33;
+let stringNumber=String(number)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
