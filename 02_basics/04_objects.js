cc64e6887eb7aff07802f41af840a4b660b2d3c1
Shreeyansh Mittal <shreeyanshmittal@gmail.com>
2025-05-11 16:50:50 +0530

    Objects completed

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+// const tinderUser=new Object()
+
+const tinderUser={}
+
+tinderUser.email="some@gmail.com"
+tinderUser.name="Shreeyansh"
+tinderUser.location="Delhi"
+// console.log(tinderUser)
+
+const regUser={
+    email:"someone@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shreeyansh",
+            lastname:"Mittal"
+        }
+    }
+}
+
+// console.log(regUser.fullname.userfullname.lastname)
+
+const obj1={1:"a",2:"b"}
+const obj2={4:"a",3:"b"}
+
+// const obj3=Object.assign({},obj1,obj2)
+
+const obj3={...obj1,...obj2}
+// console.log(obj3)
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty("location1"))
+
+const course={
+    coursename:"javascript",
+    price:"free",
+    courseInstructor:"hitesh"
+}
+
+// console.log(course.courseInstructor)
+
+const {courseInstructor: instructor}=course
+
+console.log(instructor)
